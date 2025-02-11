c8efe37a89de3c4a96e64c181018dbc02fc41dbb
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-02-10 19:13:43 -0800

    Learned Variables and Data tpyes

--- cars.py
+++ cars.py
@@ -1,2 +1,14 @@
-cars = ["porsche", "ferrari", "lamborghini", "bugatti", "koenigsegg"]
-print(cars)
+# Variables and Data Types
+
+brand = "Ford" # String
+model_year = 2020 # Integer
+price = 50000.00 # Float
+is_new = False # Boolean
+cars = ["Ford", "Toyota", "Chevrolet"] # List
+car_info = {
+    "brand": "Ford",
+    "model_year": 2020,
+    "price": 50000.00,
+    "is_new": False
+} # Dictionary
+
