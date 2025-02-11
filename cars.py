573e02e17e9477bd1c42256ebc342533867a6622
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-02-10 19:26:59 -0800

    Learned string concatenation

--- cars.py
+++ cars.py
@@ -5,10 +5,14 @@ model_year = 2020 # Integer
 price = 50000.00 # Float
 is_new = False # Boolean
 cars = ["Ford", "Toyota", "Chevrolet"] # List
-car_info = {
+car_information = {
     "brand": "Ford",
     "model_year": 2020,
     "price": 50000.00,
     "is_new": False
 } # Dictionary
 
+# String concatenation 
+car_year = "10"
+car_info = f"The car is {car_year} years old."
+print(car_info)
