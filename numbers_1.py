e29dde37fd7cdf5527dcf6fa0200d5e6e7d551fc
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-04-27 20:23:43 -0700

    Python basic number

---
+++ numbers_1.py
@@ -0,0 +1,32 @@
+age = 25
+print(age)
+# Naming convention, snake_case for variables with multiple words\
+first_name = "John"
+last_name = "Doe"
+print(first_name, last_name)
+
+# Arithmetic operations
+result = 20 / 3 # 6.66666
+print(result)
+
+print("Here is a floor version (crop deciimal):", 20 // 3) # 6
+
+# Expontents
+exponent = 2 ** 3
+print(exponent)
+
+# Rounding
+print(round(20/3, 1))
+
+# Modulus
+print(20 % 3) # 2
+
+# Addition assignment operator
+age = 10
+age += 5 # age = age + 5
+print(age) # 15
+
+number = 300 
+number /= 9
+print(number)
+
